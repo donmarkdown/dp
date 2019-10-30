0fa5fc54cec58664f2014c93db0a18bacf9f6f07
qupengcheng <18292822257@163.com>
2019-10-30 14:32:27 +0800

    装饰者、策略模式

--- src/main/java/com/demo/dp/decorator/Personable.java
+++ src/main/java/com/demo/dp/decorator/Personable.java
@@ -3,7 +3,7 @@ package com.demo.dp.decorator;
 /**
  * @ Author     ：qupengcheng
  * @ Date       ：20:38 2019/10/28
- * @ Description：
+ * @ Description：装饰者模式
  * @ Modified By：
  */
 public abstract class Personable {

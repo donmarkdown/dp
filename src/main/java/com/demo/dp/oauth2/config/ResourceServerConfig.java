b698af01c7541c09fd0509bbff8898d8ec4e2170
pcheng <reviewcode@163.com>
2019-11-20 16:24:56 +0800

    资源访问控制

--- src/main/java/com/demo/dp/oauth2/config/ResourceServerConfig.java
+++ src/main/java/com/demo/dp/oauth2/config/ResourceServerConfig.java
@@ -1,33 +1,16 @@
 package com.demo.dp.oauth2.config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
-import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
 
 /**
- * @author qupengcheng
- * @description
- * @date 21:01 2019/11/15
+ * 资源处理
+ *
+ * @author pcheng reviewcode@163.com
+ * @date 16:06 2019/11/20
  */
-
 @Configuration
 @EnableResourceServer
 public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
-
-    private static final String RESOURCE_IDS = "order";
-
-    @Override
-    public void configure(ResourceServerSecurityConfigurer resources) {
-        resources.resourceId(RESOURCE_IDS).stateless(true);
-    }
-
-    @Override
-    public void configure(HttpSecurity httpSecurity) throws Exception {
-        httpSecurity
-                .authorizeRequests()
-                .antMatchers("/api/**").authenticated();
-
-    }
 }
